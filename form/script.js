dad01a7d07dff8fbc8d3008dbc86e484b7b8eafb
pedrim202 <pedro.augusto..dr11@gmail.com>
2021-10-22 22:20:43 -0300

    Terminar JS

---
+++ form/script.js
@@ -0,0 +1,20 @@
+
+let username = document.getElementById('txtname')
+let team = document.getElementById('txtteam')
+let team2 = document.getElementById('txtteam2')
+function enviar()
+{
+
+    if(username.value.length = 0)
+    {
+        window.alert("Por favor, digite o seu nome")
+    }
+    if(team.value.length = 0)
+    {
+        window.alert("Por favor, digite o nome seu time")
+    }if(team2.value.length = 0)
+    {
+        window.alert("Por favor, digite o nome do seu time estrangeiro")
+    }
+    
+}    
